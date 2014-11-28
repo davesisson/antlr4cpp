34c89fdbb3c89465aeb21d53f5fc8b9c7dcca2a1
David Sisson <EpsilonPrime@users.noreply.github.com>
2014-11-27 16:52:26 -0800

    Fix short constructor for NoViableAltException.

--- org/antlr/v4/runtime/NoViableAltException.cpp
+++ org/antlr/v4/runtime/NoViableAltException.cpp
@@ -8,7 +8,7 @@ namespace org {
                 using org::antlr::v4::runtime::misc::NotNull;
 
 //JAVA TO C++ CONVERTER TODO TASK: Calls to same-class constructors are not supported in C++ prior to C++11:
-                NoViableAltException::NoViableAltException(Parser *recognizer) {
+                NoViableAltException::NoViableAltException(Parser *recognizer) :deadEndConfigs(nullptr), startToken(nullptr) {
                 }
 
                 NoViableAltException::NoViableAltException(Parser *recognizer, TokenStream *input, Token *startToken, Token *offendingToken, ATNConfigSet *deadEndConfigs, ParserRuleContext *ctx) : RecognitionException(recognizer, input, ctx), deadEndConfigs(deadEndConfigs), startToken(startToken) {
