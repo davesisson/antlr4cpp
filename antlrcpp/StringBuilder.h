30025fe910d7ae2ebe2de59e69336bcade5888fe
Dan McLaughlin <dan.mclaughlin@gmail.com>
2015-06-28 06:20:16 -0700

    Bring in and fix commented out "format" function.

--- antlrcpp/StringBuilder.h
+++ antlrcpp/StringBuilder.h
@@ -56,29 +56,9 @@ namespace antlrcpp {
         std::wstring::size_type length();
         
         // Utility function to provide "format" capability
-        /* Problems with this
-         static std::wstring format(const std::string fmt_str, ...) {
-         int final_n, n = (int)fmt_str.size() * 2; // reserve 2 times as much as the length of the fmt_str
-         std::string str;
-         std::unique_ptr<char[]> formatted;
-         va_list ap;
-         while(1) {
-         formatted.reset(new char[n]); // wrap the plain char array into the unique_ptr
-         strcpy(&formatted[0], fmt_str.c_str());
-         va_start(ap, fmt_str);
-         final_n = vsnprintf(&formatted[0], n, fmt_str.c_str(), ap);
-         va_end(ap);
-         if (final_n < 0 || final_n >= n)
-         n += abs(final_n - n + 1);
-         else
-         break;
-         }
-         return std::wstring((char*)formatted.get());
-         }
-         
-         static std::wstring format(const std::wstring fmt_str, ...) {
-         }
-         */
+
+        static std::wstring format(const std::string fmt_str, ...);
+        
         static std::wstring stringToWstring( std::string str );
         
         static std::string WstringTostring( std::wstring wStr );
